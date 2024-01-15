8cb6b076fea20bb6ac4ab96932b705a6ad5d8b75
koninginsamira <13108552+koninginsamira@users.noreply.github.com>
2024-01-15 03:25:18 +0100

    Remove anything between braces in title, making it optional.

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,24 @@ CONFIGFILE = os.environ['CONFIGPATH']
 # CONFIGPATH = CONFIGFILE.replace('config.yml', '')
 
 
+def remove_bracketed_text(text, brackets="()[]{}"):
+    count = [0] * (len(brackets) // 2) # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b: # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                if count[kind] < 0: # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else: # character is not a [balanced] bracket
+            if not any(count): # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars).strip()
+
+
 def upperescape(string):
     """Uppercase and Escape string. Used to help with YT-DL regex match.
     - ``string``: string to manipulate
@@ -24,8 +42,10 @@ def upperescape(string):
     string = string.replace('’',"'")
     string = string.replace('“','"')
     string = string.replace('”','"')
+    # Remove anything between braces, making it optional
+    string = remove_bracketed_text(string)
     # Treat any non-letter character as an optional non-letter wildcard
-    string = re.sub('[^A-Z]','[^A-Z]*', string)
+    string = re.sub(r'[^A-Z]','[^A-Z]*', string)
     # Make it look for and as whole or ampersands
     string = string.replace('\\ AND\\ ','\\ (AND|&)\\ ')
     return string
